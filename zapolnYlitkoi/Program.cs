69a8da184e968a48952018498246acc7842ac67c
Roninx322 <voroenckif@Mail.ru>
2015-11-17 16:16:15 +0300

    edit

--- zapolnYlitkoi/Program.cs
+++ zapolnYlitkoi/Program.cs
@@ -14,44 +14,50 @@ namespace zapolnYlitkoi
             int i;
             int j;
             int k = 1;
-            int n = mass.GetLength(0);
-            int m = mass.GetLength(1);
+            int m = mass.GetLength(0);
+            int n = mass.GetLength(1);
             Random rnd = new Random();
             i = 0;
-            j = -1;
+            j = 0;
             int x = mass.GetLength(0) * mass.GetLength(1);
             int x1 = 0;
             do
             {   
-                for (j = j+1; j < n; j++)
+                for (j = j; j < n-1; j++)
                 {
+                    x1++;
                     mass[i, j] = rnd.Next(1, 10);
-                    x1++;
+                    Class1.WriteDoubleMass(mass.GetLength(0), mass.GetLength(1), mass);
+                    Console.ReadLine();
                 }
                 
-                j--;
-                for (i = i+1; i < m; i++)
-                {
-                    x1++;
-                    mass[i, j] = rnd.Next(1, 10);  
-                }
-                i--;
-                m--;
-               
-                for (j = j-1; j >mass.GetLength(0)-n; j--)
-                {
-                    x1++;
-                    mass[i, j] = rnd.Next(1, 10);  
-                }
                 
+                for (i = i; i < m-1; i++)
+                {
+                    x1++;
+                    mass[i, j] = rnd.Next(1, 10);
+                    Class1.WriteDoubleMass(mass.GetLength(0), mass.GetLength(1), mass);
+                    Console.ReadLine();
+                }
                 n--;
-                for (i = i; i > mass.GetLength(1)-m-1; i--)
+                m--;
+                for (j = j; j>= mass.GetLength(1)-n; j--)
                 {
                     x1++;
-                    mass[i, j] = rnd.Next(1, 10);  
+                    mass[i, j] = rnd.Next(1, 10);
+                    Class1.WriteDoubleMass(mass.GetLength(0), mass.GetLength(1), mass);
+                    Console.ReadLine();
                 }
-                i++;
                 
+                for (i = i; i > mass.GetLength(0)-m-1; i--)
+                {
+                    x1++;
+                    mass[i, j] = rnd.Next(1, 10);
+                    Class1.WriteDoubleMass(mass.GetLength(0), mass.GetLength(1), mass);
+                    Console.ReadLine();
+                }
+                i++; 
+                j++;
             } while (x1 != x);
             Class1.WriteDoubleMass(mass.GetLength(0), mass.GetLength(1), mass);
             Console.ReadLine();
@@ -59,10 +65,11 @@ namespace zapolnYlitkoi
         }
         static void Main(string[] args)
         {
-            int [,] mass = new int[5,5];
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int [,] mass = new int[n,m];
             Program t = new Program();
             mass=t.create(mass);
-            
         }
     }
 }
